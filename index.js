f11edcd0d982291e8e02994b1ef0e572afa098a6
aswanipm11 <aswanipm281@gmail.com>
2023-10-30 20:04:57 +0530

    video

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// import json server
+const jsonserver=require('json-server')
+//import cors
+const cors=require('cors')
+
+//create json server
+const server=jsonserver.create()
+
+//conversion of json-js
+const middleware=jsonserver.defaults()
+
+//create a router
+const router=jsonserver.router('db.json')
+
+//apply in server
+server.use(middleware)
+server.use(cors())
+server.use(router)
+
+const PORT=5000
+server.listen(PORT,()=>{
+    console.log(`_____json server started at port :${PORT}____`);
+})
+
